32dbb5d85a30a87d259810cfc92094aab8111606
yuyu <javascript.yu@gmail.com>
2013-10-10 11:39:24 +0800

    update

--- autounsave.php
+++ autounsave.php
@@ -15,7 +15,7 @@ Author URI: http://weibo.com/1959988637
 define('AUTOUNSAVE_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 add_action('after_setup_theme','autounsave_javascript' );
 function autounsave_javascript() {
-	if(AUTOUNSAVE&&is_admin()){		
+	if(is_admin()){		
 		wp_enqueue_script('autounsave',AUTOUNSAVE_PLUGIN_URL.'autounsave.js',array('jquery'),'1.0');
 	}
 }
@@ -23,7 +23,6 @@ add_action('wp_ajax_autounsave_action', 'autounsave_action_callback');
 add_action('wp_ajax_nopriv_autounsave_action', 'autounsave_action_callback');
 function autounsave_action_callback() {
 	global $wpdb;
-	$myrows = $wpdb->get_results( "SELECT * FROM $wpdb->posts WHERE post_status='auto-draft'" );
 	$wpdb->get_results( "DELETE FROM $wpdb->posts WHERE post_status='auto-draft' OR post_type='revision'" );
 	$wpdb->get_results( "DELETE FROM $wpdb->postmeta WHERE meta_key='_edit_lock' OR meta_key='_edit_last'" );	
 	die();
